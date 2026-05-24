18e224b3e31f970c264f1df0144d803cee645fc9
murad912 <muradabdulmalik@gmail.com>
2026-05-24 14:50:03 -0500

    change commit

--- Self-check-fullstack/employe-backend/src/main/java/com/employe/employe_backend/repository/TaskRepositor.java
+++
@@ -1,9 +0,0 @@
-package com.employe.employe_backend.repository;
-
-import com.employe.employe_backend.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface TaskRepositor extends JpaRepository<Task, Long> {
-}
-
